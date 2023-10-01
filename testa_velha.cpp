eb451b4d0cdd72653cf9210105e4230657712bb8
João Pedro Fernandes Santos <jpfsantos2004@gmail.com>
2023-10-01 15:11:24 -0300

    teste 5 - indefinido

--- testa_velha.cpp
+++ testa_velha.cpp
@@ -11,24 +11,29 @@
 
 
 TEST_CASE( "Testa velha", "[single-file]" ) {
-	int teste1[3][3]= {   { 2, 0, 1 }, 
+	int teste1[3][3]= {   { 2, 0, 1 }, //testando coluna
 	                      { 2, 0, 1 },
 						  { 0, 2, 1 }
 					  };
 
-	int teste2[3][3]={    { 2, 2, 2 }, 
+	int teste2[3][3]={    { 2, 2, 2 }, //testando linha
 	                      { 1, 0, 1 },
 						  { 1, 2, 1 }
 
 					};
-	int teste3[3][3]={    { 2, 2, 1 }, 
+	int teste3[3][3]={    { 2, 2, 1 }, //testando diagonal
 	                      { 1, 2, 1 },
 						  { 1, 1, 2 }
 
 					};
-	int teste4[3][3]={    { 2, 0, 1 }, 
+	int teste4[3][3]={    { 2, 0, 1 }, //testando empate
 	                      { 0, 2, 0 },
 						  { 1, 0, 1 }
+
+					};
+	int teste5[3][3]={    { 0, 0, 0 }, //testando indefinição
+	                      { 0, 0, 0 },
+						  { 0, 0, 1 }
 
 					};
 	
@@ -36,7 +41,7 @@ TEST_CASE( "Testa velha", "[single-file]" ) {
 	REQUIRE( VerificaVelha(teste2) == 2 );
 	REQUIRE( VerificaVelha(teste3) == 2);
 	REQUIRE( VerificaVelha(teste4) == 0);
-
+	REQUIRE( VerificaVelha(teste5) == -1);
 
 }
  
